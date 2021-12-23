0dd6ce124e4c7cf07da0e3bebe6542a4946c8687
danil2326 <danil.ch99@mail.ru>
2021-12-23 19:02:22 +0400

    prom6

--- app/src/main/java/ru/gb/course1/lesson2dz/MainActivity.java
+++ app/src/main/java/ru/gb/course1/lesson2dz/MainActivity.java
@@ -3,8 +3,21 @@ package ru.gb.course1.lesson2dz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textInput;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
